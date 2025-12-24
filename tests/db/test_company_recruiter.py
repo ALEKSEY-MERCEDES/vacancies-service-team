293726e7c0c603c789b314c5190407a76964ba3a
Богословская Анна Евгеньевна <lizzylogren@mail.ru>
2025-12-24 19:45:55 +0300

    Update import path for models in test file

--- tests/db/test_company_recruiter.py
+++ tests/db/test_company_recruiter.py
@@ -1,5 +1,5 @@
 import pytest
-from infrastructure.db.models import User, Recruiter, Company, RecruiterCompany
+from src.infrastructure.db.models import User, Recruiter, Company, RecruiterCompany
 
 
 @pytest.mark.asyncio
